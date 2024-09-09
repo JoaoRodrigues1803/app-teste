3c0fa1009f36bd7b5b773425286add1eade5f09e
JoaoRodrigues1803 <joao1803208@gmail.com>
2024-09-09 11:27:50 -0300

    primeiro commit

---
+++ componentes/Tela Catalogo/TelaCatalogo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from 'react-native'
+
+
+export default function TelaCatalogo () {
+    return (
+      <View>
+        <Text>TelaCatalogo </Text>
+      </View>
+    );
+  }
